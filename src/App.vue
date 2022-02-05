e5d7b4b42dd8a39bd4c6a06f2eaeca3b39cc93c9
Ahmad <ahmadasadi37@gmail.com>
2022-02-06 00:53:25 +0100

    added fetching data from API

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,22 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'App',
   components: {
   },
+  data(){
+    return {
+      fromDate: "2021-12-31",
+      toDate: "2022-01-31",
+    }
+  },
+  async created(){
+    const { data } = await axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${this.fromDate}&end=${this.toDate}&index=[USD]`);
+    console.log(data);
+  }
 }
 </script>
 
