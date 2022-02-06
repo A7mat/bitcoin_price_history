01ca37a97bd7a52e482ad1e79f7bf083f6c6decf
Ahmad <ahmadasadi37@gmail.com>
2022-02-06 02:07:18 +0100

    added input fields & dynamic date range for API fetching

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
-  <div id="app">
-
+  <div id="app container">
+    <div class="row mt-5">
+      <div class="col text-center">
+        <h1>Bitcoin Price Tracker</h1>
+      </div>
+    </div>
+    <div class="row m-5">
+      <div class="col">
+        <input type="date" v-model="fromDate">
+      </div>
+      <div class="col">
+        <input type="date" v-model="toDate">
+      </div>
+      <div class="col">
+        <input type="button" value="render" @click="updateData()">
+      </div>
+    </div>
+    <div class="row m-5">
+      {{data}}
+    </div>
   </div>
 </template>
 
@@ -13,13 +31,25 @@ export default {
   },
   data(){
     return {
-      fromDate: "2021-12-31",
+      fromDate: "2022-01-01",  // To-Do: shouldn't be hardcoded
       toDate: "2022-01-31",
+      data: null,
     }
   },
-  async created(){
-    const { data } = await axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${this.fromDate}&end=${this.toDate}&index=[USD]`);
-    console.log(data);
+  mounted () {
+    axios
+      .get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${this.fromDate}&end=${this.toDate}&index=[USD]`)
+      .then(response => (this.data = response.data.bpi))
+      .catch(error => console.log(error))
+
+  },
+  methods: {
+    updateData(){
+      axios
+      .get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${this.fromDate}&end=${this.toDate}&index=[USD]`)
+      .then(response => (this.data = response.data.bpi))
+      .catch(error => console.log(error))
+    }
   }
 }
 </script>
